fe35b5939f0f78da60fba27bf656f4e644c54586
Alooi <alnasser@purdue.edu>
2019-04-17 20:41:14 -0400

    Stable moving pet

--- main_screen.js
+++ main_screen.js
@@ -6,45 +6,68 @@ canvas.width = window.innerWidth;
 var c = canvas.getContext('2d');
 
 
-//var playereName = prompt("Please enter your name", "Enter your name here");
 
-var petType = 'dog';
-var petImage = new Image();
-petImage.src = "images/dog.jpg";
 
+
+//var playereName = prompt("Whats your name", "Enter name here");
+
+//var petType = prompt("which pet you want?", "Pet here");
+
+var petType = "dog";
+
+function draw_everthing_else()
+{
+  this.backgroundDraw = function()
+  {
+    var backgroundImage = new Image();
+    backgroundImage.src = "images/roomPic.jpg";
+    c.drawImage(backgroundImage,0,0,canvas.width,canvas.height);
+    //console.log(backgroundImage);
+  }
+
+  this.healthBarDraw = function()
+  {
+    var healthBar = new Image();
+    healthBar.src = "images/healthBar.jpg";
+    c.drawImage(healthBar,20,30,200,200);
+  }
+}
 
 function pet(petType,x,y){
-    console.log("here0");
+    this.x = x;
+    this.y = y;
+    this.xv = 1;
+    var petImage = new Image();
 
-    //var petImage = "images/" + petType + ".jpg";
-    
-  
   this.draw = function(){
-    c.clearRect(0,0,innerWidth, innerHeight);
-    c.drawImage(petImage,x,y);
-    console.log(petImage);
+    //console.log(this.x,this.y);
+    c.drawImage(petImage,this.x,this.y,250,250);
+    petImage.src = "./images/" + petType + ".jpg";
+    
   }
 
 
   this.update = function(){
-    //x++;
-    //y++;
+    if (this.x == canvas.width-250 || this.x == 0)
+    {
+      this.xv = -this.xv;
+    }
+    this.x += this.xv;
+    //this.y++;
     this.draw();
-    console.log("update");
-    //requestAnimationFrame(update)
   }
-
-  this.update();
 
 }
 
-
+petObject = new pet(petType,(canvas.width/2),(canvas.height)-250)
+everythingElse = new draw_everthing_else();
 
 function animate(){
-  //requestAnimationFrame(animate)
+  requestAnimationFrame(animate)
   c.clearRect(0,0,innerWidth, innerHeight);
-  this.pet(petType,10,10);
-
+  everythingElse.healthBarDraw();
+  everythingElse.backgroundDraw();
+  petObject.update();
 }
 
 animate();
